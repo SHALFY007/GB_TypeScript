55c54379e2154014d63343ad5d64ac511afa8aeb
vladislav <vl_evlashin@mail.ru>
2023-01-20 19:57:59 +0300

    Complete first part of first work

--- src/index.ts
+++ src/index.ts
@@ -15,21 +15,24 @@ let nextTrip:Date = new Date(
   now.getMonth(),
   now.getDate() + 4
 )
-
-const formInfo: SearchFormData = {
-  inputDate: currentDate,
-  outPutDate: nextTrip,
-  maxPrice: 10
-}
+// 
 
 window.addEventListener('DOMContentLoaded', () => {
   localStorage.setItem('user', JSON.stringify({username: "Wade Warren", avatarUrl: "/img/avatar.png"}))
   localStorage.setItem('favoritesAmount', '/img/avatar.png')
-
-  search(formInfo)
   renderUserBlock(getUserData().username, getUserData().avatarUrl, getFavoritesAmount())
   renderSearchFormBlock(currentDate, nextTrip)
   renderSearchStubBlock()
+  const a:HTMLInputElement = document.querySelector('.max-price')
+  document.querySelector('button').addEventListener('click', e => {
+    e.preventDefault()
+    const formInfo: SearchFormData = {
+      inputDate: currentDate,
+      outPutDate: nextTrip,
+      maxPrice: a.value
+    }
+    search(formInfo)
+  })
   renderToast(
       {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
       {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
--- src/search.ts
+++ src/search.ts
@@ -1,9 +1,27 @@
+import {renderSearchResultsBlock, renderSearchResultsBlockOne} from './search-results.js'
+
 export interface SearchFormData {
     inputDate: Date,
     outPutDate: Date,
-    maxPrice: number
+    maxPrice: string
   }
 
-export function search(SearchFormData):void {
-    console.log(SearchFormData)
+let asd = []
+
+export async function search(SearchFormData:SearchFormData):Promise<void> {
+  if (asd.length>0) asd = [] 
+  for (let i=1; i <=4; i++) {
+    await fetch(`http://localhost:3030/places/${i}`)
+    .then(data => data.json())
+    .then(res => {
+      console.log(res)
+      if (res.price <= +SearchFormData.maxPrice) {
+        asd.push(res)
+      }
+    })
+  }
+  renderSearchResultsBlock()
+  asd.forEach(e => renderSearchResultsBlockOne(e))
+  
+  
 }
